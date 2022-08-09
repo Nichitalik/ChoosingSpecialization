340a9079cc2daf8356feca8bb3ec99062b6d8629
Nikita Balenko <nikitabalenko2013@gmail.com>
2022-08-10 01:18:52 +0300

    Написали код решение задачи

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿String[] arr1 = new String [] {"hello", "2", "world", ":-)"};
+
+String[] deleteString(String[] arr)
+{
+    int count = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    String[] result = new String [count];
+    count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result[count] = arr[i];
+            count++;
+        }
+    }
+    return result;
+}
+
+System.Console.WriteLine(String.Join(", ", deleteString(arr1)));
+
